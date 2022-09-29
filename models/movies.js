49df1d2e7f7ae5a44bb949d21b04290f8620f8ad
unknown <eam618334@yandex.ru>
2022-09-29 15:45:27 +0300

    feat: сделать модели

---
+++ models/movies.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: { //длительность фильма
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+    length: 4,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    // match: /^https?:\/\/(www\.)?[-\w@:%.+~#=]{1,256}\.[a-z0-9()]{2,}\b([-\w()@:%.+~#=\\?&]*)/i,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    match: /^https?:\/\/(www\.)?[-\w@:%.+~#=]{1,256}\.[a-z0-9()]{2,}\b([-\w()@:%.+~#=\\?&]*)/i,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    match: /^https?:\/\/(www\.)?[-\w@:%.+~#=]{1,256}\.[a-z0-9()]{2,}\b([-\w()@:%.+~#=\\?&]*)/i,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('user', movieSchema);
