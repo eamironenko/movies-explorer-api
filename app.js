f94599710dcd2a72de7423d1a556d6d0efdeffe2
unknown <eam618334@yandex.ru>
2022-09-30 09:25:53 +0300

    feat: добавить logger

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
+const { requestLogger, errorLogger} = require('./middlewares/logger');
+app.use(requestLogger);
 
+
+
+
+app.use(errorLogger);
+//другие ошибки
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`); // сервер на 3000 порту
