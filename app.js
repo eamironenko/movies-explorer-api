d8d048beab56093d222dbd0c72cc0fe3b9a2d06b
unknown <eam618334@yandex.ru>
2022-09-30 17:32:08 +0300

    feat: добавить middlewares auth

--- app.js
+++ app.js
@@ -9,13 +9,39 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 const { requestLogger, errorLogger} = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
+const NotFoundPage = require('./errors/NotFoundPage');
+
+//перед точками входа
 app.use(requestLogger);
 
+//роутеры, которые не требуют авторизации
 
 
+//роутеры, которые требуют авторизации
 
+
+app.use('*', auth, () => {
+  throw new NotFoundPage('Страница не найдена');
+});
+
+
+//логгер ошибок
 app.use(errorLogger);
-//другие ошибки
+app.use(errors());
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`); // сервер на 3000 порту
